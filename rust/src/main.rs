4a716c7d7a5bb27cc2a2603019b302c25816a1cf
Erwin de Haan <1627021+EraYaN@users.noreply.github.com>
2023-08-20 17:27:58 +0200

    Add rust template endpoint

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,27 @@
 use axum::{
+    extract::Query,
+    response::Html,
     routing::get,
     http::StatusCode,
-    Json, Router, extract::Query, Extension
+    Json, Router, Extension
 };
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use sqlx::mysql::{MySqlPoolOptions, MySqlPool};
+use tera::Tera;
+use tera::Context;
 
 #[tokio::main]
 async fn main() {
+
+    let tera: Tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+
     // initialize tracing
     tracing_subscriber::fmt::init();
 
@@ -24,7 +37,9 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root))
         .route("/barcode/lookup", get(barcode_lookup))
-        .layer(Extension(pool));
+        .route("/barcode/template", get(barcode_template))
+        .layer(Extension(pool))
+        .layer(Extension(tera));
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -40,11 +55,8 @@ async fn root() -> (StatusCode, &'static str) {
     return (StatusCode::OK, "This is an API server, please use the correct client.")
 }
 
-async fn barcode_lookup(
-    Query(payload): Query<BarcodeInput>, Extension(pool): Extension<MySqlPool>, 
-) -> (StatusCode, Result<Json<BarcodeModel>, String>) {
-
-    let barcode : Option<BarcodeModel> = sqlx::query_as!(
+async fn get_barcode(barcode: String, pool: MySqlPool) -> Option<BarcodeModel>{
+    return sqlx::query_as!(
         BarcodeModel,
         r#"
         select 
@@ -52,16 +64,41 @@ async fn barcode_lookup(
         barcode         `code: String`,
         status          `status: BarcodeType`
         from barcodes where barcode = ?"#,
-        payload.barcode
+        barcode
     )
     .fetch_optional(&pool).await.unwrap();
+}
 
-    return match barcode {
+async fn barcode_lookup(
+    Query(payload): Query<BarcodeInput>, Extension(pool): Extension<MySqlPool>, 
+) -> (StatusCode, Result<Json<BarcodeModel>, String>) {
+    return match get_barcode(payload.barcode, pool).await {
         Some(item) => (StatusCode::OK, Ok(Json(item))),
         None => (StatusCode::NOT_FOUND, Err(String::from("Not Found")))
     }
+}
 
-    
+async fn barcode_template(
+    Query(payload): Query<BarcodeInput>, Extension(pool): Extension<MySqlPool>, Extension(engine): Extension<Tera>,
+) -> (StatusCode, Result<Html<String>, Html<String>>) {
+
+    let barcode : Option<BarcodeModel> = get_barcode(payload.barcode, pool).await;
+
+    let mut context = Context::new();
+
+    if let Some(item) = barcode {
+        context.insert("barcode", &item);
+        return match engine.render("barcode.html", &context) {
+            Ok(output) => (StatusCode::OK, Ok(Html(output))),
+            Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Err(Html(err.to_string())))
+        }
+    } else {
+        context.insert("err", "no such barcode");
+        return match engine.render("not-found.html", &context) {
+            Ok(output) => (StatusCode::NOT_FOUND, Ok(Html(output))),
+            Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Err(Html(err.to_string())))
+        }
+    }
 }
 
 #[derive(Deserialize)]
@@ -88,3 +125,15 @@ struct BarcodeModel {
     code: String,
     status: BarcodeType,
 }
+
+#[derive(Serialize)]
+pub struct PageModel {
+    barcode: Option<BarcodeModel>
+}
+
+#[derive(Serialize)]
+pub struct ErrPageModel {
+    err: &'static str
+}
+
+
