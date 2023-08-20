e600ef2951b85f77932beba5e9ff68f46236f495
Erwin de Haan <1627021+EraYaN@users.noreply.github.com>
2023-08-20 17:27:46 +0200

    Add python template endpoint

--- python/src/main.py
+++ python/src/main.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 
 import mysql.connector
 import os
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from typing import Union
 
 app = FastAPI()
 
 dbconfig = {
-    "host": os.getenv("MYSQL_HOST", "percona"),
+    "host": os.getenv("MYSQL_HOST", "server.e.dehaan.family"),
     "user": os.getenv("MYSQL_USER", "root"),
     "password": os.getenv("MYSQL_PASSWORD", "testpassword"),
     "database": os.getenv("MYSQL_DATABASE", "speedtest"),
@@ -18,6 +20,8 @@ dbconfig = {
 dbpool = mysql.connector.pooling.MySQLConnectionPool(pool_name = "speedtest",
                                                       pool_size = 20, # docker file has 5 workers, 100 connections in total
                                                       **dbconfig)
+
+templates = Jinja2Templates(directory="templates")
 
 @app.get("")
 async def root():
@@ -32,4 +36,14 @@ async def scan(barcode: Union[str, None] = None):
             return {"id":obj[0],"code":obj[1],"status":obj[2]}
         else:
             raise HTTPException(status_code=404, detail="Barcode not found")
+        
+@app.get("/barcode/template", response_class=HTMLResponse)
+async def scan(request: Request, barcode: Union[str, None] = None):
+    with dbpool.get_connection() as cnx, cnx.cursor(buffered=True) as cur:        
+        cur.execute("select barcode_id as id, barcode as code, status as status from barcodes where barcode = %s", (barcode,))
+        obj = cur.fetchone()
+        if obj is not None:
+            return templates.TemplateResponse("barcode.html.j2", {"request": request, "barcode": {"id":obj[0],"code":obj[1],"status":obj[2]}})
+        else:
+            return templates.TemplateResponse("not-found.html.j2", {"request": request, "err": "no such barcode"})
 
